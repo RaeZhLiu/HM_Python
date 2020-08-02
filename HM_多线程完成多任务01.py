088256e63fc218a311dcaa073d43d4b42c13d4f9
liuzhihui <liuzhihui0911@gmail.com>
2020-08-02 16:23:53 +0800

    多线程学习一：多线程完成多任务

---
+++ HM_多线程完成多任务01.py
@@ -0,0 +1,33 @@
+import threading
+import time
+
+
+def sing():
+    sing_current_thread = threading.current_thread()
+    print("sing_current_thread", sing_current_thread)
+    for i in range(3):
+        print("singing....")
+        time.sleep(0.3)
+
+
+def dance():
+    dance_current_thread = threading.current_thread()
+    print("dance_current_thread", dance_current_thread)
+    for i in range(3):
+        print("dancing....")
+        time.sleep(0.3)
+
+
+def main():
+    main_current_thread = threading.current_thread()
+    print("main_current_thread:", main_current_thread)
+    # 1. 创建子线程
+    sing_thread = threading.Thread(target=sing, name="sing_thread")
+    dance_thread = threading.Thread(target=dance, name="dance_thread")
+    # 2. 启动子线程
+    sing_thread.start()
+    dance_thread.start()
+
+
+if __name__ == '__main__':
+    main()
